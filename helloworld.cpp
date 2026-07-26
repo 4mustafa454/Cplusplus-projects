c2334912060948fd01307fff46fbfd21bcf4e089
4mustafa454 <4mustafa454@gmail.com>
2026-07-26 15:47:03 -0500

    Day 1 C++

---
+++ helloworld.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    //This is a comment
+    /*
+      This 
+      is 
+      a 
+      multiline comment
+    */
+    std::cout << "I like biryani" << '\n';
+    std::cout << "Its really good!" << '\n';
+    return 0;
+
+}
